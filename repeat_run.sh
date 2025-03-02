9ef2591d98df8ffa664fd34a61c95cc3edd1e021
stone <github@stone-bo.com>
2025-03-02 02:16:07 +0000

    revise(exp): modification of configuration in repeat_run

--- repeat_run.sh
+++ repeat_run.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-MODEL="ConvNeXtTiny"
-for b in $(seq 10 40 530); do
-  ID_xMem="${MODEL}-${b}-xMem"
-  ID_xMem_LLM="${MODEL}-${b}-LLM"
-  ID_xMem_LLM_CUDA="${MODEL}-${b}-CUDA"
-  python xProfile.py -m "$MODEL" -b "$b" -o "SGD" -u -g 8 -r "${ID_xMem}"
-  sleep 5
-  python tProfile.py -m "$MODEL" -b "$b" -o "SGD" -u -g 8 -r "${ID_xMem_LLM}"
-  sleep 5
-  python tProfile.py -m "$MODEL" -b "$b" -o "SGD" -c -g 8 -r "${ID_xMem_LLM_CUDA}"
-  sleep 5
+MODELS = (
+  "VGG11"
+  "VGG16"
+  "VGG19"
+  "ResNet50"
+  "ResNet101"
+  "ResNet152"
+  "MobileNetV2"
+  "MobeNetV3Small"
+  "MobeNetV3Large"
+  "MnasNet"
+  "ConvNeXtTiny"
+  "ConvNeXtBase"
+  "RegNetX400MF"
+  "RegNetX32GF"
+  "RegNetY400MF"
+  "RegNetY32GF"
+)
+#MODELS=("ConvNeXtTiny" "ResNet50" "EfficientNetB0")
+optimizers = ("SGD", "Adam", "RMSprop", "Adagrad", "AdamW")
+for MODEL in "${MODELS[@]}"; do
+  for b in $(seq 10 40 530); do
+    ID_xMem_LLM="${MODEL}-${b}-LLM"
+    python tProfile.py -m "$MODEL" -b "$b" -o "SGD" -u -g 8 -r "${ID_xMem_LLM}"
+    sleep 5
+  done
 done
