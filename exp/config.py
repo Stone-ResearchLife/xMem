9632115fcbc381873ff00f9b8f281b70a7460363
stone <github@stone-bo.com>
2025-05-08 18:00:06 +0100

    refactor(exp): update llm conf

--- exp/config.py
+++ exp/config.py
@@ -5,37 +5,54 @@ from ures.tools.enum import EnumManipulator
 
 class ExperimentConfig(BaseModel):
     """Configuration for the experiment."""
-    batch_range: tuple[int] = Field(default=(100, 200), description="Batch size for the experiment.")
-    optimisers: list[str] = Field(default=["AdamW"], description="Optimiser to use for the experiment.")
+
+    batch_range: tuple[int] = Field(
+        default=(100, 200), description="Batch size for the experiment."
+    )
+    optimisers: list[str] = Field(
+        default=["AdamW"], description="Optimiser to use for the experiment."
+    )
     run_id: str = Field(default="default_run", description="Run ID for the experiment.")
     models: list[str] = Field(default=[], description="Models to use.")
-    repeats: int = Field(default=1, description="Number of times to run the experiment.")
+    repeats: int = Field(
+        default=1, description="Number of times to run the experiment."
+    )
     gpu_id: int = Field(default=0, description="GPU ID to use.")
     debug: bool = Field(default=False, description="Enable debug mode.")
 
 
 class CNNExperiments(ExperimentConfig):
     """Configuration for CNN experiments."""
-    batch_range: tuple[int] = Field(default=(200, 1000, 100), description="Batch size for the experiment.")
+
+    batch_range: tuple[int] = Field(
+        default=(200, 1000, 100), description="Batch size for the experiment."
+    )
     run_id: str = Field(default="CNN-Exp", description="Run ID for the experiment.")
     optimisers: list[str] = Field(
         default=["SGD", "Adam", "RMSprop", "Adagrad", "AdamW"],
-        description="Optimiser to use for the experiment."
+        description="Optimiser to use for the experiment.",
     )
-    models: list[str] = Field(default=EnumManipulator(AllModels).fetch_keys(), description="List of CNN models.")
+    models: list[str] = Field(
+        default=EnumManipulator(AllModels).fetch_keys(),
+        description="List of CNN models.",
+    )
 
 
 class TransformerExperiments(ExperimentConfig):
     """Configuration for transformer experiments."""
-    batch_range: tuple[int] = Field(default=(5, 70, 5), description="Batch size for the experiment.")
-    run_id: str = Field(default="Transformer-Exp", description="Run ID for the experiment.")
+
+    batch_range: tuple[int] = Field(
+        default=(5, 70, 5), description="Batch size for the experiment."
+    )
+    run_id: str = Field(
+        default="Transformer-Exp", description="Run ID for the experiment."
+    )
     models: list[str] = Field(
         default=[
             "EleutherAI/gpt-neo-125M",
             "facebook/opt-125m",
             "facebook/opt-350m",
             "cerebras/Cerebras-GPT-111M",
-            # "microsoft/deberta-base",
             "T5-small",
             "t5-base",
             "distilbert/distilgpt2",
@@ -45,5 +62,5 @@ class TransformerExperiments(ExperimentConfig):
     )
     optimisers: list[str] = Field(
         default=["SGD", "Adam", "AdamW"],
-        description="Optimiser to use for the experiment."
+        description="Optimiser to use for the experiment.",
     )
