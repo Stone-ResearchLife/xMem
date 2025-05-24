32db42c99f4b69f8c63bf5cc1080b2dc1b0f52a1
stone <github@stone-bo.com>
2025-05-24 11:46:15 +0100

    refactor(exp): update colab_run

--- exp/colab_run.sh
+++ exp/colab_run.sh
@@ -2,17 +2,20 @@
 
 NUMBER_OF_RUNS=5
 MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
-OPTIMIZER="Adafactor"
+OPTIMIZERS=("Adafactor" "AdamW" "SGD", "Adam")
 BATCH=1
 
-for i in $(seq 1 $NUMBER_OF_RUNS)
-do
-    echo "Run #$i of $NUMBER_OF_RUNS"
-    python run_in_colab.py --model ${MODEL} --bs ${BATCH} --optimizer ${OPTIMIZER}
 
-    # If it's not the last run and DELAY_SECONDS is greater than 0, then sleep.
-    if [ "$i" -lt "$NUMBER_OF_RUNS" ] && [ "$DELAY_SECONDS" -gt 0 ]; then
-        echo "Waiting for $DELAY_SECONDS seconds before the next run..."
-        sleep "$DELAY_SECONDS"
-    fi
+for OPTIMIZER in "${OPTIMIZERS[@]}"
+  for i in $(seq 1 $NUMBER_OF_RUNS)
+  do
+      echo "Run #$i of $NUMBER_OF_RUNS"
+      python run_in_colab.py --model ${MODEL} --bs ${BATCH} --optimizer ${OPTIMIZER}
+
+      # If it's not the last run and DELAY_SECONDS is greater than 0, then sleep.
+      if [ "$i" -lt "$NUMBER_OF_RUNS" ] && [ "$DELAY_SECONDS" -gt 0 ]; then
+          echo "Waiting for $DELAY_SECONDS seconds before the next run..."
+          sleep "$DELAY_SECONDS"
+      fi
+  done
 done
